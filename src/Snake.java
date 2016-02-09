a51ffff74ca9469077cc78a82f318ad9ec180c53
MarshallRich <rlmarsh@g.clemson.edu>
2016-02-09 11:09:42 -0500

    create animal classes

---
+++ src/Snake.java
@@ -0,0 +1,13 @@
+/**
+ * Created by MacLap on 2/9/16.
+ */
+public class Snake extends Reptile {
+    public Snake() {
+        this.name = "Snake";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ssssss");
+    }
+}
